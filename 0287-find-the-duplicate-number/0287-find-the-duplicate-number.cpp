fe527e5aa053f0673055df5c91db19d84c492072
VIXXEN <u865528@gmail.com>
2026-08-04 00:03:58 +0530

    Time: 4 ms (60.97%), Space: 67.8 MB (21.11%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        vector<int> arr( nums.size() + 1 , 0);
+        for( int i = 0 ; i < nums.size() ;i++){
+            arr[nums[i]]++;
+            if(arr[nums[i]] > 1){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
