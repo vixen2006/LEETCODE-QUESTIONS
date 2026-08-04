5562b042d0ae90eafbd55dbb9656999a6b310bd6
VIXXEN <u865528@gmail.com>
2026-08-04 18:31:38 +0530

    Time: 46 ms (12.19%), Space: 45.1 MB (10.82%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
 
-    void find_combination(int idx, int target,
-                      vector<int>& arr,
-                      vector<vector<int>>& ans,
-                      vector<int>& ds) {
+    void find_combination(int idx , int target , vector<int>& arr , vector<vector<int>>& ans , vector<int> ds){
+       
 
     if (idx == arr.size()) {
         if (target == 0) {
@@ -13,21 +11,23 @@ public:
         return;
     }
 
-    if (arr[idx] <= target) {
-        ds.push_back(arr[idx]);
-        find_combination(idx, target - arr[idx], arr, ans, ds);
-        ds.pop_back();
+        //pick up the element 
+
+        if( arr[idx] <= target){
+            ds.push_back(arr[idx]);
+            find_combination( idx , target - arr[idx] , arr , ans , ds);
+            ds.pop_back();
+        }
+
+        find_combination(idx + 1 , target , arr , ans , ds);
     }
-
-    find_combination(idx + 1, target, arr, ans, ds);
-}
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> ds;
+        vector<int> ds ;
         vector<vector<int>> ans ;
 
-        find_combination(0 , target ,candidates, ans , ds);
+        find_combination(0 , target , candidates , ans , ds);
 
-        return ans ;        
+        return ans ;
+        
     }
 };
