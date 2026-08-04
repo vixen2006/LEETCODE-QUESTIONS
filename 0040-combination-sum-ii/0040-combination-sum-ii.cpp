63873658325228d8be524dcdb5385c1343b797bd
VIXXEN <u865528@gmail.com>
2026-08-04 22:28:19 +0530

    Time: 0 ms (100%), Space: 14 MB (65.44%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void find_combination(int idx, int target, vector<int>& arr,
+                          vector<vector<int>>& ans, vector<int>& ds) {
+
+        // Base case
+        if (target == 0) {
+            ans.push_back(ds);
+            return;
+        }
+
+        // Try every element from idx onwards
+        for (int i = idx; i < arr.size(); i++) {
+
+            // Skip duplicates
+            if (i > idx && arr[i] == arr[i - 1])
+                continue;
+
+            // Since array is sorted, no need to continue
+            if (arr[i] > target)
+                break;
+
+            // Pick the element
+            ds.push_back(arr[i]);
+
+            // Move to next index because one element can be used only once
+            find_combination(i + 1, target - arr[i], arr, ans, ds);
+
+            // Backtrack
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+
+        sort(candidates.begin(), candidates.end());
+
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        find_combination(0, target, candidates, ans, ds);
+
+        return ans;
+    }
+};
