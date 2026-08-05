42218c47f7fa04260775ee717a0e2d9fe2ece2f0
VIXXEN <u865528@gmail.com>
2026-08-05 12:44:58 +0530

    Time: 2 ms (44.41%), Space: 14.1 MB (65.49%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    void find_combination(int idx, int target, vector<int>& arr,
-                          vector<vector<int>>& ans, vector<int>& ds) {
 
-        // Base case
-        if (target == 0) {
+    void find_combination(int idx , int target , vector<int>& arr , vector<vector<int>>& ans , vector<int>& ds){
+
+        // base case 
+        if( target == 0){
             ans.push_back(ds);
-            return;
+            return ;
         }
-        for (int i = idx; i < arr.size(); i++) {
 
-            // Skip duplicates
-            if (i > idx && arr[i] == arr[i - 1])
-                continue;
 
-            // Since array is sorted, no need to continue
-            if (arr[i] > target)
-                break;
+        for( int i = idx ; i < arr.size() ; i++){
+
+            // skip  duplicates 
+
+            if( i > idx && arr[i] == arr[i-1]){
+                continue ;
+            }
+
+            if(arr[i] > target) break ;
 
             ds.push_back(arr[i]);
-
-            find_combination(i + 1, target - arr[i], arr, ans, ds);
-
+            find_combination(i+ 1 , target-arr[i] , arr , ans , ds);
             ds.pop_back();
+
         }
     }
-
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-
-        sort(candidates.begin(), candidates.end());
-
-        vector<vector<int>> ans;
+        vector<vector<int>> ans ;
         vector<int> ds;
 
-        find_combination(0, target, candidates, ans, ds);
+        sort(candidates.begin() , candidates.end());
 
-        return ans;
+        find_combination(0 , target , candidates , ans , ds);
+
+        return ans ;
+        
     }
 };
