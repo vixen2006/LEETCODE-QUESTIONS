22206b4b662789dd750acc410a09080bf1e13463
VIXXEN <u865528@gmail.com>
2026-08-05 21:13:36 +0530

    Time: 0 ms (100%), Space: 10.3 MB (97.5%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    void find_subsets(int idx , vector<int>& arr , vector<vector<int>>& ans , vector<int>& ds){
+        ans.push_back(ds);
+
+        for( int i =  idx ; i < arr.size() ; i++){
+
+            if(i > idx && arr[i] == arr[i-1]) continue;
+
+            // //dont take nums[idx]
+            // find_subsets(i+1 , arr , ans , ds);
+            
+            // all unique
+            ds.push_back(arr[i]);
+            find_subsets(i+1 , arr , ans , ds);
+            ds.pop_back();
+
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+
+        sort(nums.begin() , nums.end());
+
+        vector<vector<int>> ans ;
+        vector<int> ds ;
+
+        find_subsets(0 , nums , ans , ds);
+
+        return ans ;
+        
+    }
+};
