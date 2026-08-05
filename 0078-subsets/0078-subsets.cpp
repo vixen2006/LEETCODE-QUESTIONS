b61f2efdff99c27d5bc45cb8816d4d03914b7667
VIXXEN <u865528@gmail.com>
2026-08-05 14:04:33 +0530

    Time: 3 ms (27.16%), Space: 12.6 MB (24.93%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,61 +1,29 @@
 class Solution {
 public:
+    void create_subsets(int idx, vector<int>& nums,
+                        vector<vector<int>>& ans,
+                        vector<int>& ds) {
 
-    void create_subsets(int idx , vector<int>& nums , vector<vector<int>>& ans , vector<int>& ds){
-       class Solution {
-public:
-
-    void create_subsets(int idx , vector<int>& nums , vector<vector<int>>& ans , vector<int>& ds){
-        // ans.push_back(ds);
-
-        if( idx == nums.size()){
+        if (idx == nums.size()) {
             ans.push_back(ds);
-            return ;
+            return;
         }
 
-        //dont take nums[idx]
-        create_subsets(idx + 1 , nums , ans , ds);
-        
-        //take nums[idx]
+        // Don't take nums[idx]
+        create_subsets(idx + 1, nums, ans, ds);
+
+        // Take nums[idx]
         ds.push_back(nums[idx]);
-
-        create_subsets(idx + 1 , nums , ans , ds);
-
+        create_subsets(idx + 1, nums, ans, ds);
         ds.pop_back();
     }
+
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans ;
-        vector<int> ds ;
+        vector<vector<int>> ans;
+        vector<int> ds;
 
-        create_subsets(0 , nums , ans , ds);
+        create_subsets(0, nums, ans, ds);
 
-        return ans ;
-        
-    }
-};
-
-        if( idx == nums.size()){
-            ans.push_back(ds);
-            return ;
-        }
-
-        //dont take nums[idx]
-        create_subsets(idx + 1 , nums , ans , ds);
-        
-        //take nums[idx]
-        ds.push_back(nums[idx]);
-
-        create_subsets(idx + 1 , nums , ans , ds);
-
-        ds.pop_back();
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans ;
-        vector<int> ds ;
-
-        create_subsets(0 , nums , ans , ds);
-
-        return ans ;
-        
+        return ans;
     }
 };
