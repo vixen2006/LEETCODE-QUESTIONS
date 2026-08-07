ec30105c96e4f9df6b3cc73452d35925de916f5a
VIXXEN <u865528@gmail.com>
2026-08-07 13:54:50 +0530

    Time: 0 ms (100%), Space: 8.8 MB (57.64%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+
+        // generating arr with all the numbers 
+        
+        // we need no of nos in arr for that we hv fact 
+
+        vector<int> arr ;
+        int fact = 1 ;
+
+        for( int i = 1 ; i < n ; i++){
+            fact *= i;
+            arr.push_back(i);
+        } 
+
+        arr.push_back(n);
+
+        string ans = "";
+
+        k -= 1 ;
+
+        while(true){
+             ans += to_string(arr[k/fact]);
+
+             arr.erase(arr.begin() + k/fact);
+
+             if(arr.size() == 0) break ;
+
+             k = k% fact ;
+
+             fact = fact / arr.size();
+        }
+        
+        return ans ;
+    }
+};
