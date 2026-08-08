3fba13a59ecc0d72c7636a38d49bd0dc0641bc9a
VIXXEN <u865528@gmail.com>
2026-08-08 16:39:32 +0530

    Time: 190 ms (83.96%), Space: 10.5 MB (81.44%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool backtrack( vector<vector<char>>& board , string& word , int i , int j , int idx){
+
+        if (idx == word.length()) return true;
+
+        // conditions for i & j  , when not ideal
+
+        if( i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[idx]){
+            return false ;
+        }
+
+        // to avoid repeating 
+        // stroring values as marked
+
+        char temp = board[i][j];
+
+        board[i][j] = '#'; // # is demo
+
+        bool found = backtrack( board , word , i+ 1 , j , idx + 1)|| backtrack( board , word , i - 1 , j , idx + 1) || backtrack( board , word , i , j+1 , idx + 1) || backtrack( board , word , i , j-1 , idx + 1);
+
+        board[i][j] = temp ;
+
+        return found ;
+    }
+
+
+    bool exist(vector<vector<char>>& board, string word) {
+
+        int m = board.size() ; // rows
+        int n = board[0].size() ; // colms
+
+        for( int i = 0 ; i < m ; i ++){
+            for ( int j = 0 ; j < n ; j++){
+                if( backtrack( board , word , i , j , 0) )  return true ;
+            }
+        }
+
+        return false ;
+        
+    }
+};
