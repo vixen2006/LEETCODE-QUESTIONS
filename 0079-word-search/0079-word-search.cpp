ec624fe287c1bff4f9eb6537784c1d03cbad16ca
VIXXEN <u865528@gmail.com>
2026-08-08 16:41:33 +0530

    Time: 192 ms (76.26%), Space: 10.4 MB (93.1%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -27,8 +27,8 @@ public:
 
     bool exist(vector<vector<char>>& board, string word) {
 
-        int m = board.size() ; // rows
-        int n = board[0].size() ; // colms
+        int m = board.size() ; 
+        int n = board[0].size() ; 
 
         for( int i = 0 ; i < m ; i ++){
             for ( int j = 0 ; j < n ; j++){
