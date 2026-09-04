be46a013ee718f12a469a4347a16fd21aadf25cd
VIXXEN <u865528@gmail.com>
2026-09-04 12:18:47 +0530

    Time: 3 ms (25.17%), Space: 31 MB (66.39%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        int mn , mx , mini = INT_MAX;
-        for( int i = 0 ; i < nums.size() ;i++){
-            mx = *max_element(nums.begin() , nums.begin()+i);
-            mn = *min_element(nums.begin()+i , nums.end());
-            if(mx-mn > k) continue;
-            else{
-                mini = min(mini , i);
-            }
-            return mini;
+        int n = nums.size();
+        vector<int> stb(n); // stb =  stability score arr
+        stb[0] = nums[0];
 
-        }
-        
-        return -1 ;
-        
+        for( int i = 1; i < n ; i++){
+            stb[i] = max(nums[i] , stb[i-1]); 
+        }  
+
+        int mn = INT_MAX;
+        for(int i = n-1 ; i >= 0 ;i--){
+            mn = min(mn , nums[i]);
+            stb[i] -= mn ;
+        }  
+        for(int i = 0; i<n;i++){
+            if(stb[i]<=k) return i;
+        }  return -1;   
     }
-};
+}; //tc = O(n)
+
+
+// // tc = O(N2)
+
+// class Solution {
+// public:
+//     int firstStableIndex(vector<int>& nums, int k) {
+//         int mn , mx , mini = INT_MAX;
+//         for( int i = 0 ; i < nums.size() ;i++){
+//             mx = *max_element(nums.begin() , nums.begin()+i);
+//             mn = *min_element(nums.begin()+i , nums.end());
+//             if(mx-mn > k) continue;
+//             else{
+//                 mini = min(mini , i);
+//             }
+//             return mini;
+
+//         }
+        
+//         return -1 ;
+        
+//     }
+// };
